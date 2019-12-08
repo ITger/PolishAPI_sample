392a4bf5651243d1ac1b3cc14b80e6b8a82e8cee
Piotr Zerynger <p.zerynger@gmail.com>
2019-12-08 18:44:33 +0100

    .

--- server/src/main/java/pl/itger/PolishAPI/PolishAPI_2_1_2_SpringBoot.java
+++ server/src/main/java/pl/itger/PolishAPI/PolishAPI_2_1_2_SpringBoot.java
@@ -1,31 +1,26 @@
 package pl.itger.PolishAPI;
 
-import org.jetbrains.annotations.NotNull;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@EnableMongoRepositories(
-//            basePackageClasses = {AccountInfoRepository.class, HoldInfoRepository.class, TransactionInfoRepository.class})
-
 @SpringBootApplication
 @ComponentScan(basePackages = {"pl.itger.dataFaker", "pl.itger.JWTokens", "pl.itger.PolishAPI"})
 @EnableSwagger2
-public class PolishAPI_2_1_2_SpringBoot implements CommandLineRunner {
+public class PolishAPI_2_1_2_SpringBoot { //implements CommandLineRunner {
 
     public static void main(String[] args) {
         new SpringApplication(PolishAPI_2_1_2_SpringBoot.class).run(args);
     }
 
-    @Override
+    /*@Override
     public void run(@NotNull String... arg0) {
         if (arg0.length > 0 && arg0[0].equals("exitcode")) {
             throw new ExitException();
         }
-    }
+    }*/
 
     class ExitException
             extends RuntimeException
