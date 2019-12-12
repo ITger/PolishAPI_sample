6a59be29d910fcc4e26145ba683081d2a6c53b10
Piotr Zerynger <p.zerynger@gmail.com>
2019-12-12 17:01:19 +0100

    .

--- server/src/main/java/pl/itger/PolishAPI/ServletInitializer.java
+++ server/src/main/java/pl/itger/PolishAPI/ServletInitializer.java
@@ -1,11 +1,8 @@
 package pl.itger.PolishAPI;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-public class ServletInitializer extends SpringBootServletInitializer {
-    @Override
+public class ServletInitializer {// extends SpringBootServletInitializer {
+   /* @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(PolishAPI_2_1_2_SpringBoot.class);
-    }
+    }*/
 }
