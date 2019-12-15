7d81fcfb85ce07c204fc925c2648ed324df6fa0b
Piotr Zerynger <p.zerynger@gmail.com>
2019-12-15 12:54:53 +0100

    .

--- server/src/main/java/pl/itger/PolishAPI/PolishAPI_2_1_2_SpringBoot.java
+++ server/src/main/java/pl/itger/PolishAPI/PolishAPI_2_1_2_SpringBoot.java
@@ -1,40 +1,40 @@
 package pl.itger.PolishAPI;
 
-import org.jetbrains.annotations.NotNull;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.ExitCodeGenerator;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
 @SpringBootApplication
 @ComponentScan(basePackages = {"pl.itger.dataFaker", "pl.itger.JWTokens", "pl.itger.PolishAPI"})
 @EnableSwagger2
-public class PolishAPI_2_1_2_SpringBoot implements CommandLineRunner {
+public class PolishAPI_2_1_2_SpringBoot {//implements CommandLineRunner {
 
     public static void main(String[] args) {
-        new SpringApplication(PolishAPI_2_1_2_SpringBoot.class).run(args);
+        //new
+        SpringApplication.run(PolishAPI_2_1_2_SpringBoot.class, args);
     }
 
-    @Override
-    public void run(@NotNull String... arg0) {
-        if (arg0.length > 0 && arg0[0].equals("exitcode")) {
-            throw new ExitException();
-        }
+//    @Override
+//    public void run(@NotNull String... arg0) {
+//        if (arg0.length > 0 && arg0[0].equals("exitcode")) {
+//            throw new ExitException();
+//        }
+//
+//        //CodecProvider pojoCodecProvider = PojoCodecProvider.builder().register("pl.itger.PolishAPI.io.swagger.model").build();
+//        //CodecRegistry pojoCodecRegistry = fromRegistries( fromProviders(pojoCodecProvider));
+//    }
 
-        //CodecProvider pojoCodecProvider = PojoCodecProvider.builder().register("pl.itger.PolishAPI.io.swagger.model").build();
-        //CodecRegistry pojoCodecRegistry = fromRegistries( fromProviders(pojoCodecProvider));
-    }
-
-    class ExitException
-            extends RuntimeException
-            implements ExitCodeGenerator {
-        private static final long serialVersionUID = 1L;
-
-        @Override
-        public int getExitCode() {
-            return 10;
-        }
-    }
+//    class ExitException
+//            extends RuntimeException
+//            implements ExitCodeGenerator {
+//        private static final long serialVersionUID = 1L;
+//
+//        @Override
+//        public int getExitCode() {
+//            return 10;
+//        }
+//    }
 }
--- server/src/main/java/pl/itger/PolishAPI/ServletInitializer.java
+++
@@ -1,8 +0,0 @@
-package pl.itger.PolishAPI;
-
-public class ServletInitializer {// extends SpringBootServletInitializer {
-   /* @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(PolishAPI_2_1_2_SpringBoot.class);
-    }*/
-}
